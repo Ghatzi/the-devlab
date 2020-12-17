f622c6dbae0bb640bf7866b245c141be5576b3f4
George Hatzi <g_hatzi@hotmail.com>
2020-12-17 22:56:06 +0000

    config changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   env: {
     REACT_APP_INSTAGRAM_ACCESS_TOKEN:
-      'IGQVJYRXFuaEJJelFFa0ZAvb3ZAXRURkM3dyejVZAb1FNUjE5RnpaeGVsYWhiMTd2cVBIeVQzRGRyS1J4Ri1ubkxENmFGMnpvYkJMbkY4MjV5bDF4bFotQlZAXeEdWSjMwb1ZAJcGsydXRzNXg3dlRDbzc3agZDZD',
-    EMAIL: 'contact@the-devlab.co.uk',
-    PASSWORD: 'brsvGS7!1%4w'
+      process.env.REACT_APP_INSTAGRAM_ACCESS_TOKEN,
+    EMAIL: process.env.EMAIL,
+    PASSWORD: process.env.PASSWORD
   }
 };
