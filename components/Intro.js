da785da1881bab45ec8e0df2c6688a7327fe98c1
George Hatzi <g_hatzi@hotmail.com>
2020-12-19 16:39:35 +0000

    styling changes

--- components/Intro.js
+++ components/Intro.js
@@ -4,12 +4,15 @@ const Intro = () => (
   <section className="intro-feature" id="intro">
     <h1>Welcome to the-DevLab</h1>
     <p className="mt-4 mb-4">
-      Hello, My name is George Hatzi and I'm a Frontend developer from London.
+      Hello, my name is George Hatzi and I'm a Frontend developer from London.
+    </p>
+
+    <p className="mb-4">
       the-DevLab was developed as a place where I can showcase some of my work
-      to future recruiters or clients. During my time as a developer I have
-      worked with many languages including PHP, ASP, JavaScript, jQuery, XSLT,
-      CSS and more however my main areas of focus currently are React, Next.js,
-      SASS &amp; Node.js.
+      to recruiters and clients. During my time as a developer I have worked
+      with many languages including PHP, ASP, JavaScript, jQuery, XSLT, CSS and
+      more however my main areas of focus currently are React, Next.js, SASS
+      &amp; Node.js.
     </p>
     <p>
       Below you will find my GitHub repositories as well as the latest Instagram
